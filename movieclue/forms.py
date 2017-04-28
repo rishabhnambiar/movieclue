a3108332ea32a5fbbaf077c6e5582e2c1bee22f3
Rishabh Nambiar <rishabhn@protonmail.com>
2017-04-28 19:27:46 +0530

    Fixed ChoiceField issues for Chrome and Firefox.

--- movieclue/forms.py
+++ movieclue/forms.py
@@ -4,7 +4,8 @@ from django import forms
 class SearchMovieForm(forms.Form):
     search = forms.CharField(label='', max_length=25, widget=forms.TextInput(
         attrs={'class': 'form-control'}))
-    choices = [('movie', 'MOVIES'),
+
+    choices = [('movie', 'MOVIES ⇩'),
                ('series', 'TV')]
 
     choice = forms.ChoiceField(label='', choices=choices, widget=forms.Select(
